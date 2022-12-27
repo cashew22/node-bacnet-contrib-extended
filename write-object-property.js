82162da27804024af8ff49cb19956c251acd7054
Eric Tremblay <etremblay@distech-controls.com>
2022-12-27 11:08:33 -0500

    Rework write-object-property to remove a bit useless default value settings and little clean up

--- write-object-property.js
+++ write-object-property.js
@@ -9,61 +9,32 @@ module.exports = function (RED) {
         const node = this;
         this.name = config.name;
 
-        node.status({fill: 'green', shape: 'dot', text: 'active'});
-
         node.on('input', function (msg) {
-            var randNum = Math.floor(Math.random() * 7100);
-            msg.communicationPort = 47808 + randNum;
-
-            if (msg.interface === "" || msg.interface === null) {
-                msg.interface = "0.0.0.0";
-            }
-
-            if (msg.broadcastAddress === "" || msg.broadcastAddress === null) {
-                msg.broadcastAddress = "255.255.255.255";
-            }
-
-            if (msg.apduTimeout === "" || msg.apduTimeout === null) {
-                msg.apduTimeout = 7000;
-            }
-
-            if (msg.reuseAddr === "" || msg.reuseAddr === null) {
-                msg.reuseAddr = true;
-            }
-
-            if (msg.address === "" || msg.address === null) {
-                msg.address = "address is invalid";
-                node.send(msg);
-                return;
-            }
+            node.status({fill: 'green', shape: 'dot', text: 'Sending...'});
 
             var bacnetClient = new Bacnet({
-                port: msg.communicationPort,
+                port: 47808 + Math.floor(Math.random() * 7100),
                 interface: msg.interface,
                 broadcastAddress: msg.broadcastAddress,
                 apduTimeout: msg.apduTimeout,
                 reuseAddr: msg.reuseAddr
             });
 
-            console.log("msg.address ----> ", msg.address);
-            console.log("msg.objectId ---> ", msg.objectId);
-            console.log("msg.propertyId -> ", msg.propertyId);
-            console.log("msg.values -----> ", msg.values);
-
             try {
                 bacnetClient.writeProperty(msg.address, msg.objectId, msg.propertyId, msg.values, function (err, value) {
                     if (err !== null) {
+                        node.status({fill: 'red', shape: 'dot', text: 'error'});
                         msg.payload = err;
-                        node.send(msg);
                     } else {
+                        node.status({fill: 'green', shape: 'dot', text: 'Success'});
                         msg.payload = value;
-                        node.send(msg);
                     }
                 });
             } catch (e) {
+                node.status({fill: 'red', shape: 'dot', text: 'error'});
                 msg.payload = e;
-                node.send(msg);
             }
+            node.send(msg);
         });
     }
     RED.nodes.registerType("write-object-property", WriteObjectProperty);
