5a7ed437635c68c736929226a4efe82fc2d6d4ca
Eric Tremblay <etremblay@distech-controls.com>
2023-01-02 21:16:54 -0500

    Cleanup read-objects-list-within-device
    Note to self, this node should go, it can be done with the read property
    node. If we want a wrapper for some properties, we should pack them in
    a special node.

--- read-objects-list-within-device.js
+++ read-objects-list-within-device.js
@@ -1,151 +1,44 @@
-module.exports = function(RED) {
-
-    const Bacnet = require('node-bacnet')
+module.exports = function (RED) {
+    "use strict";
 
     function ReadObjectsListWithinDevice(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
 
-        this.name = config.name
-
+        const Bacnet = require('node-bacnet');
         const node = this;
+        this.name = config.name;
 
-        node.status({ fill: 'green', shape: 'dot', text: 'active' })
+        node.on('input', function (msg) {
+            node.status({fill: 'green', shape: 'dot', text: 'Sending...'});
 
-
-
-        node.on('input', function(msg) {
-
-
-
-          console.log("deviceId ---> uuuu ");
-
-          console.log("deviceId ---> ", msg.deviceId);
-
-
-
-          if(msg.communicationPort == "" || msg.communicationPort == null){
-
-            var randNum = Math.floor(Math.random() * 7100);
-            msg.communicationPort = 47808 + randNum;
-
-          }
-  
-          if(msg.interface == "" || msg.interface == null){
-  
-            msg.interface = "0.0.0.0";
-          }
-  
-          if(msg.broadcastAddress == "" || msg.broadcastAddress == null){
-  
-            msg.broadcastAddress = "255.255.255.255";
-          }
-  
-          if(msg.apduTimeout == "" || msg.apduTimeout == null){
-  
-            msg.apduTimeout = 7000;
-          }
-  
-          if(msg.reuseAddr == "" || msg.reuseAddr == null){
-            msg.reuseAddr = true;
-          }
-  
-          
-
-
-          if(msg.transportClosedDuration == "" || msg.transportClosedDuration == null){
-
-            msg.transportClosedDuration = 90000;
-          }
-
-          if(msg.deviceId == "" || msg.deviceId == null){
-
-            msg.payload = "deviceId is invalid";
-            node.send(msg);
-            return;
-          }
-
-          if(msg.address == "" || msg.address == null){
-
-            msg.payload = "address is invalid";
-            node.send(msg);
-            return;
-          }
-
-
-
-
-          var bacnetClient = new Bacnet({ 
-            port: msg.communicationPort,                          // Use BAC0 as communication port
-            interface: msg.interface,          // Listen on a specific interface
-            broadcastAddress: msg.broadcastAddress,  // Use the subnet broadcast address
-            apduTimeout: msg.apduTimeout,
-            reuseAddr: msg.reuseAddr                     
-           });
-
-           console.log("deviceId ---> ", msg.deviceId);
-
-
-
-
-           const requestArray = [
-            { objectId: { type: 8, instance: msg.deviceId }, properties: [{ id: 76 }] },
-            ];
-
-
-
-            try{
-
-              bacnetClient.readPropertyMultiple(msg.address, requestArray, (err, value) => {
-
-    
-
-                if (err !== null) {
-                    bacnetClient.close();
-                    msg.payload = err
-                    node.send(msg);
-                }
-            
-                else{
-                    bacnetClient.close();
-                    msg.payload = value.values[0].values[0].value;
-                    node.send(msg);
-                }
-            
-            
+            var bacnetClient = new Bacnet({
+                port: msg.communicationPort,
+                interface: msg.interface,
+                broadcastAddress: msg.broadcastAddress,
+                apduTimeout: msg.apduTimeout,
+                reuseAddr: msg.reuseAddr
             });
 
+            const requestArray = [
+                {objectId: {type: 8, instance: msg.deviceId}, properties: [{id: 76}]}
+            ];
+
+            try {
+                bacnetClient.readPropertyMultiple(msg.address, requestArray, function (err, value) {
+                    if (err !== null) {
+                        node.status({fill: 'red', shape: 'dot', text: 'error'});
+                        msg.payload = err;
+                    } else {
+                        node.status({fill: 'green', shape: 'dot', text: 'Success'});
+                        msg.payload = value.values[0].values[0].value;
+                    }
+                    bacnetClient.close();
+                    node.send(msg);
+                });
+            } catch (e) {
+                console.log(e);
             }
-
-            catch(e){
-
-              console.log(e);
-
-            }
-
-
-
-
-
-
-
-
-
-
-            console.log("test test 123");
-
-
-
         });
-
-
-
-
-
-
-
-        
-
-
     }
     RED.nodes.registerType("read-objects-list-within-device", ReadObjectsListWithinDevice);
-}
+};
